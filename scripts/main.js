6ee46f02161636c3dbb3263f4406eb3c06ac88b8
ydede404 <dakitse@gmail.com>
2017-07-06 18:10:20 +0200

    Upload exercice 1

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+document.getElementById("elementscentraux").onclick = function(){
+	document.getElementById("elementscentraux").style.visibility="hidden";
+	document.getElementById("afficher").style.visibility="visible";
+	document.getElementById("afficher1").style.visibility="visible";
+	document.getElementById("afficher2").style.visibility="visible";
+	document.getElementById("afficher3").style.visibility="visible";
+	document.getElementById("afficher4").style.visibility="visible";
+}
